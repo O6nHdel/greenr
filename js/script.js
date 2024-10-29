56391aac8e7b95783020d7b692e2c43bac789ebb
O6nHdel <hugo.delaporte@gmail.com>
2024-10-29 13:43:12 +0100

    add responsive

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Fonction pour basculer le menu déroulant
+function toggleMenu() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.classList.toggle('active'); // Ajoute ou enlève la classe 'active'
+}
